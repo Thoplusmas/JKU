aabba286f7085d6e6c328d2420fd10c9f2329968
Florian Daniel <florian.daniel@carployee.com>
2020-01-01 14:22:29 +0100

    .gitignore, booking.controller.ts und tsconfig.json aktualisiert

--- HotelService/src/booking/booking.controller.ts
+++ HotelService/src/booking/booking.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Body, Post } from '@nestjs/common';
-import { ApiUseTags } from '@nestjs/swagger';
+import { ApiUseTags, ApiCreatedResponse, ApiBadRequestResponse } from '@nestjs/swagger';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { Booking } from './booking.entity';
 import { BookingService } from './room.service';
@@ -10,6 +10,8 @@ export class BookingController {
     constructor(private readonly bookingService: BookingService) { }
 
     @Post()
+    @ApiCreatedResponse({ type: Booking, description: 'Returns newly created booking' })
+    @ApiBadRequestResponse({ description: 'Bad Request: E.G If some invalid parameters were added or missing' })
     public async createBooking(@Body() createBookingDto: CreateBookingDto): Promise<Booking> {
         return this.bookingService.createBooking(createBookingDto);
     }
