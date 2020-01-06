e561b4bcd3efda6c55f705504a3ee7ca67c17d83
simonkroissmayr <simonkroissmayr@gmail.com>
2020-01-06 21:20:26 +0100

    get & getByID update

--- HotelService/src/room/room.controller.ts
+++ HotelService/src/room/room.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Body, Post, Param } from '@nestjs/common';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { RoomService } from './room.service';
-import { ApiCreatedResponse, ApiBadRequestResponse, ApiImplicitBody, ApiUseTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiBadRequestResponse, ApiImplicitBody, ApiUseTags, ApiNoContentResponse, ApiOkResponse, ApiModelProperty, ApiImplicitParam, ApiNotFoundResponse } from '@nestjs/swagger';
 import { Room } from './room.entity';
 
 @Controller('room')
@@ -17,13 +17,18 @@ export class RoomController {
     }
 
     @Get()
+    @ApiOkResponse({ type: Array(Room), isArray:true,  description: 'Returns all rooms' })
+    @ApiNoContentResponse({ description: 'No rooms in database' })
     public async getAll(): Promise<Array<Room>> {
         return this.roomService.getAll();
     }
 
     @Get(':id')
+    @ApiOkResponse({ type: Room, description: 'Returns the room with the given ID' })
+    @ApiNoContentResponse({ description: 'Given ID not in database' })
+    @ApiBadRequestResponse({ description: 'Bad Request: E.G If some invalid parameters were added or missing' })
+    @ApiImplicitParam({name:'id', type:'number'})
     public async getOne(@Param('id')id): Promise<Room> {
         return this.roomService.getOne(id);
-    }
-
+    } 
 }
